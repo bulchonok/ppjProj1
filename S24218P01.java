abd3cfe7f0d7be74fa080ef3e8bf792ebbdaa14f
Kiryl <102504342+bulchonok@users.noreply.github.com>
2022-05-06 22:27:23 +0200

    v1.4

--- S24218P01.java
+++ S24218P01.java
@@ -137,41 +137,388 @@ class S24218P01 {
             System.out.println();
         }
 
+                int x1,y1, ThisColor;
 
-        boolean isChosen=false;
+        boolean isChosen=false,isChecker;
         char charSc;
-        int intSc;
+        int intSc, CountTurn=0;
+        while(!isChosen){
         System.out.println("choose your checker");
         charSc= scannerChar.next().charAt(0);//char scanner
         intSc= scannerInt.nextInt();
-        while(!isChosen){
 
-        //paste appr 50-120 //check cords provided //get arguments: colorIsCorret, is checker(or queen), is on board
 
-        if(isChecker){
-            System.out.println("choose action:");//max 3 for checker
+                    b1 = black1;      //important thing to reset value  //////!!!!! add check if checker isn't beaten
+                    for (int k = 0; k < 6; k++) {
+                        x = (int) (b1 % 8) + 1;
+                        b1 = b1 >> 3;
+                        y = (int) (b1 % 8) + 1;
+                        b1 = b1 >> 3;
+
+                        s1=(int) (b1%2);
+                        b1=b1>>1;
+                        s2=(int) (b1%2);
+                        b1=b1>>1;
+                        s3=(int) (b1%2);
+                        b1=b1>>1;
+
+
+                        if ((s3==1)&&(x == intSc) && (y == ((int)charSc-64)) ){
+                            if(s1==CountTurn%2){
+                                switch(s2){
+                                    case 1->{
+                                        isChecker=false;
+                                        x1=intSc;
+                                        y1=(int)charSc-64;
+                                        ThisColor=s1;
+                                    }
+                                    case 0->{
+                                        isChecker=true;
+                                        x1=intSc;
+                                        y1=(int)charSc-64;
+                                        ThisColor=s1;
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+
+
+                    b2 = black2;      //important thing to reset value
+                    for (int k = 0; k < 6; k++) {
+                        x = (int) (b2 % 8) + 1;
+                        b2 = b2 >> 3;
+                        y = (int) (b2 % 8) + 1;
+                        b2 = b2 >> 3;
+
+                        s1=(int) (b2%2);
+                        b2=b2>>1;
+                        s2=(int) (b2%2);
+                        b2=b2>>1;
+                        s3=(int) (b2%2);
+                        b2=b2>>1;
+
+                        if ((s3==1)&&(x == intSc) && (y == ((int)charSc-64)) ){
+                            if(s1==CountTurn%2){
+                                switch(s2){
+                                    case 1->{
+                                        isChecker=false;
+                                        x1=intSc;
+                                        y1=(int)charSc-64;
+                                        ThisColor=s1;
+                                    }
+                                    case 0->{
+                                        isChecker=true;
+                                        x1=intSc;
+                                        y1=(int)charSc-64;
+                                        ThisColor=s1;
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+
+
+                    w1 = white1;      //important thing to reset value
+                    for (int k = 0; k < 6; k++) {
+                        x = (int) (w1 % 8) + 1;
+                        w1 = w1 >> 3;
+                        y = (int) (w1 % 8) + 1;
+                        w1 = w1 >> 3;
+
+                        s1=(int) (w1%2);
+                        w1=w1>>1;
+                        s2=(int) (w1%2);
+                        w1=w1>>1;
+                        s3=(int) (w1%2);
+                        w1=w1>>1;
+
+                        if ((s3==1)&&(x == intSc) && (y == ((int)charSc-64)) ){
+                            if(s1==CountTurn%2){
+                                switch(s2){
+                                    case 1->{
+                                        isChecker=false;
+                                        x1=intSc;
+                                        y1=(int)charSc-64;
+                                        ThisColor=s1;
+                                    }
+                                    case 0->{
+                                        isChecker=true;
+                                        x1=intSc;
+                                        y1=(int)charSc-64;
+                                        ThisColor=s1;
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+
+
+                    w2 = white2;      //important thing to reset value
+                    for (int k = 0; k < 6; k++) {
+                        x = (int) (w2 % 8) + 1;
+                        w2 = w2 >> 3;
+                        y = (int) (w2 % 8) + 1;
+                        w2 = w2 >> 3;
+
+                        s1=(int) (w2%2);
+                        w2=w2>>1;
+                        s2=(int) (w2%2);
+                        w2=w2>>1;
+                        s3=(int) (w2%2);
+                        w2=w2>>1;
+
+
+                        if ((s3==1)&&(x == intSc) && (y == ((int)charSc-64)) ){
+                            if(s1==CountTurn%2){
+                                switch(s2){
+                                    case 1->{
+                                        isChecker=false;
+                                        x1=intSc;
+                                        y1=(int)charSc-64;
+                                        ThisColor=s1;
+                                    }
+                                    case 0->{
+                                        isChecker=true;
+                                        x1=intSc;
+                                        y1=(int)charSc-64;
+                                        ThisColor=s1;
+                                    }
+                                }
+                            }
+                        }
+
+
+                    }
+            System.out.println("wrong field");
+
+
+        }
+
+            int MoveNotMade;
+            while(MoveNotMade==0){
+                if(isChecker){
+            System.out.println("make a move:");//max 3 for checker
             charSc= scannerChar.next().charAt(0);//char scanner
             intSc= scannerInt.nextInt();
-//paste appr 50-120 //check cords provided //get arguments: colorIsCorret,
-            //if y1= y2-1-->check if path isn't blocked
+                if (x1==intSc && y1==((int)charSc-64)-1){
+                    MoveNotMade=0;
+                            b1 = black1;      //important thing to reset value  //////!!!!! add check if checker isn't beaten
+                            for (int k = 0; k < 6; k++) {
+                                x = (int) (b1 % 8) + 1;
+                                b1 = b1 >> 3;
+                                y = (int) (b1 % 8) + 1;
+                                b1 = b1 >> 3;
+
+                                s1=(int) (b1%2);
+                                b1=b1>>1;
+                                s2=(int) (b1%2);
+                                b1=b1>>1;
+                                s3=(int) (b1%2);
+                                b1=b1>>1;
+
+
+                                if (x==x1&&(y1+1)==y&&y!=8) {     //check if there is a pon
+                                    System.out.println("wrong move: path blocked");
+                                    MoveNotMade++;
+                                }
+                            }
+
+
+
+                            b2 = black2;      //important thing to reset value
+                            for (int k = 0; k < 6; k++) {
+                                x = (int) (b2 % 8) + 1;
+                                b2 = b2 >> 3;
+                                y = (int) (b2 % 8) + 1;
+                                b2 = b2 >> 3;
+
+                                s1=(int) (b2%2);
+                                b2=b2>>1;
+                                s2=(int) (b2%2);
+                                b2=b2>>1;
+                                s3=(int) (b2%2);
+                                b2=b2>>1;
+
+                                if (x==x1&&(y1+1)==y&&y!=8) {     //check if there is a pon
+                                    System.out.println("wrong move: path blocked");
+                                    MoveNotMade++;
+                                }
+                            }
+
+
+
+                            w1 = white1;      //important thing to reset value
+                            for (int k = 0; k < 6; k++) {
+                                x = (int) (w1 % 8) + 1;
+                                w1 = w1 >> 3;
+                                y = (int) (w1 % 8) + 1;
+                                w1 = w1 >> 3;
+
+                                s1=(int) (w1%2);
+                                w1=w1>>1;
+                                s2=(int) (w1%2);
+                                w1=w1>>1;
+                                s3=(int) (w1%2);
+                                w1=w1>>1;
+
+                                if (x==x1&&(y1+1)==y&&y!=8) {     //check if there is a pon
+                                    System.out.println("wrong move: path blocked");
+                                    MoveNotMade++;
+                                }
+                            }
+
+
+
+                            w2 = white2;      //important thing to reset value
+                            for (int k = 0; k < 6; k++) {
+                                x = (int) (w2 % 8) + 1;
+                                w2 = w2 >> 3;
+                                y = (int) (w2 % 8) + 1;
+                                w2 = w2 >> 3;
+
+                                s1=(int) (w2%2);
+                                w2=w2>>1;
+                                s2=(int) (w2%2);
+                                w2=w2>>1;
+                                s3=(int) (w2%2);
+                                w2=w2>>1;
+
+
+                                if (x==x1&&(y1+1)==y&&y!=8) {     //check if there is a pon
+                                    System.out.println("wrong move: path blocked");
+                                    MoveNotMade++;
+                                }
+
+                            }
+
+                }
+
+
+
+
             //if y1= y2-2 && x1=x2-2--> check if pass isn't blocked and there is a checkler of different color in the x1+1 y1+1 point
 
             if (){
                 //change pos + action
-
+        //s3==1 && s1!=ThisColor
                 isChosen=true;
             }
 
 
         }else{
-
+            System.out.println("make a move:");//max 3 for checker
+            charSc= scannerChar.next().charAt(0);//char scanner
+            intSc= scannerInt.nextInt();
+//paste appr 50-120 //check cords provided //get arguments: colorIsCorret,
             //...
             isChosen=true;
 
         }
 
-        }
-        isChosen=false;
+        }///change place
+
+                            long buffer=0;
+                            b1 = black1;      //important thing to reset value  //////!!!!! add check if checker isn't beaten
+                            for (int k = 0; k < 6; k++) {
+                                x = (int) (b1 % 8) + 1;
+                                b1 = b1 >> 3;
+                                y = (int) (b1 % 8) + 1;
+                                b1 = b1 >> 3;
+
+                                s1=(int) (b1%2);
+                                b1=b1>>1;
+                                s2=(int) (b1%2);
+                                b1=b1>>1;
+                                s3=(int) (b1%2);
+                                b1=b1>>1;
+
+                                
+
+
+                                if ((s3==1)&&(x ==x1) && (y == y1)) {     //check if there is a pon
+
+                                    buffer=buffer+(((((((s3<<1)+s2)<<1)+s1)<<3)+(charSc-64))<<3)+intSc;
+
+                                }else{
+                                    buffer=buffer+(((((((s3<<1)+s2)<<1)+s1)<<3)+y)<<3)+x;
+                                }
+
+
+                            }
+
+
+
+                            b2 = black2;      //important thing to reset value
+                            for (int k = 0; k < 6; k++) {
+                                x = (int) (b2 % 8) + 1;
+                                b2 = b2 >> 3;
+                                y = (int) (b2 % 8) + 1;
+                                b2 = b2 >> 3;
+
+                                s1=(int) (b2%2);
+                                b2=b2>>1;
+                                s2=(int) (b2%2);
+                                b2=b2>>1;
+                                s3=(int) (b2%2);
+                                b2=b2>>1;
+
+                                if ((s3==1)&&(x == j+1) && (y == i+1)) {     //check if there is a pon
+                                    System.out.print('\u2659'+ " ");
+                                    turn = true;
+                                }
+                            }
+
+
+
+                            w1 = white1;      //important thing to reset value
+                            for (int k = 0; k < 6; k++) {
+                                x = (int) (w1 % 8) + 1;
+                                w1 = w1 >> 3;
+                                y = (int) (w1 % 8) + 1;
+                                w1 = w1 >> 3;
+
+                                s1=(int) (w1%2);
+                                w1=w1>>1;
+                                s2=(int) (w1%2);
+                                w1=w1>>1;
+                                s3=(int) (w1%2);
+                                w1=w1>>1;
+
+                                if ((s3==1)&&(x == j+1) && (y == i+1)) {     //check if there is a pon
+                                    System.out.print('\u265F'+ " ");
+                                    turn = true;
+                                }
+                            }
+
+
+
+                            w2 = white2;      //important thing to reset value
+                            for (int k = 0; k < 6; k++) {
+                                x = (int) (w2 % 8) + 1;
+                                w2 = w2 >> 3;
+                                y = (int) (w2 % 8) + 1;
+                                w2 = w2 >> 3;
+
+                                s1=(int) (w2%2);
+                                w2=w2>>1;
+                                s2=(int) (w2%2);
+                                w2=w2>>1;
+                                s3=(int) (w2%2);
+                                w2=w2>>1;
+
+
+                                if ((s3==1)&&(x == j+1) && (y == i+1)) {     //check if there is a pon
+                                    System.out.print('\u265F'+ " ");
+                                    turn = true;
+                                }
+
+
+                            }
+           // find x1y1 and change to intsc chrac
 
 
 
